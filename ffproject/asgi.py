2762f163bfa198d6ba7b7f2e91c765f0068eaa9e
ducanhitz <ducanhng.dev@gmail.com>
2021-09-13 15:56:47 +0700

    change sth

--- ffproject/asgi.py
+++ ffproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FProject project.
+ASGI config for ffproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ffproject.settings')
 
 application = get_asgi_application()
--- ffproject/wsgi.py
+++ ffproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FProject project.
+WSGI config for ffproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ffproject.settings')
 
 application = get_wsgi_application()
