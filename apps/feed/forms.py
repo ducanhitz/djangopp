7136d8a21e99d3b7015b2e3f927e81e0ff7621b9
ducanhitz <ducanha1k51@gmail.com>
2021-10-25 15:14:39 +0700

    sth

--- apps/feed/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from apps.feed.models import Post
-
-
-class NewPostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['description', 'pic', 'tags']
-        
--- apps/helloworld/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import Index
-
-
-urlpatterns = [
-    path('', Index.as_view()),
-]
