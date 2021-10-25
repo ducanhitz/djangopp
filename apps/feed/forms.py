3701e0d3b4aaeb41fb4cb481dbb45ed4d2ea2404
ducanhitz <ducanha1k51@gmail.com>
2021-10-25 15:32:02 +0700

    create post and show your post

---
+++ apps/feed/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from apps.feed.models import Post
+
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['description', 'pic', 'tags']
+        
