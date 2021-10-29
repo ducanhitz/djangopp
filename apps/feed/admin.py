b8c25be6090adf337c3c5aa2524098c192db6986
ducanhitz <ducanha1k51@gmail.com>
2021-10-29 15:58:27 +0700

    create post and show your post

---
+++ apps/feed/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from apps.feed.models import Post
+
+# Register your models here.
+admin.site.register(Post)
--- ffproject/settings.py
+++ ffproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.helloworld',
     'apps.users',
+    'apps.feed',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +127,11 @@ AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -139,3 +146,5 @@ try:
     from ffproject.local_setting import *
 except ImportError:
     pass
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
